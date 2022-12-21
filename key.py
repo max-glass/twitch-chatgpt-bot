d391778c7be909047c238f74beccb84bfed88336
Max <107602115+max-glass@users.noreply.github.com>
2022-12-21 08:28:46 -0500

    Delete key.py

--- key.py
+++
@@ -1,7 +0,0 @@
-
-CLIENT_ID     = "gzdiyofmijb9vd3dfop7kc85detlxv" # "k0i7zq0lw1i0cy6kpeqyiyw1shwlxe"
-CLIENT_SECRET = "wo8yzdob5x635s0r2e8oh5gj6k90pl" # "x41s1ozfji1tquwbexezt76a2800k6"
-API_KEY       = "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" # "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"
-OAUTH_TOKEN   = "oauth:6rgn5ne18vimbinkbkwa5f7cbezmw1" # "oauth:brnyc6p1p6toobuak1ymq4qsj2p8jk"
-USERNAME      = "OpenAIChatGPT"
-CHANNEL_NAME  = "OpenAIChatGPT"
